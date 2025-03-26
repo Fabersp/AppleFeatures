bd3b6b2a8e864bfe158f143ac7042b9db459604d
Fabricio Padua <fabricio_0505_@hotmail.com>
2025-03-26 13:02:29 -0400

    first push

---
+++ AppleFeatures/AppleFeatures/AppleFeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFeaturesApp.swift
+//  AppleFeatures
+//
+//  Created by Fabricio Padua on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleFeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
